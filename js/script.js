ac94ca887d7a7374e526e33e1a23ebd634e343a0
Akas_Datta <012akasdatta@gmail.com>
2025-04-16 17:09:28 +0600

    done slider part

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
-
-
+// Swiper js 
+document.addEventListener("DOMContentLoaded", function () {
+  var swiper = new Swiper(".default-carousel", {
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+});
 
 
 
